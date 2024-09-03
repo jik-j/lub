192e94b877b268f738ebc0bc3ce33899709cc720
jik-j <rgsugan574@gmail.com>
2024-09-03 23:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("start-btn").addEventListener("click", function() {
+    document.querySelector(".hero").classList.add("hidden");
+    document.getElementById("message-section").classList.remove("hidden");
+});
